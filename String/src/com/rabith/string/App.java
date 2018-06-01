3ded767c925dc1b50d4f1d23308f2c8425b3e6c0
Rabith Kuniyil <>
2018-06-01 04:13:06 -0400

    String - Get getSuffixes

--- String/src/com/rabith/string/App.java
+++ String/src/com/rabith/string/App.java
@@ -1,9 +1,18 @@
 package com.rabith.string;
 
+import java.util.List;
+
 public class App {
 	public static void main(String[] args) {
 		StringOperation operation = new StringOperation();
-		System.out.println(operation.reverseString("Rabith Kuniyil")); 
+		System.out.println("Reverse String =>" + operation.reverseString("Rabith Kuniyil"));
+		System.out.println("----");
+
+		List<String> suffixes = operation.getSuffixes("Rabith Kuniyil");
+		for (String s : suffixes)
+			System.out.println("String Suffixes =>" + s);
+		System.out.println("----");
+
 	}
 
 }
--- String/src/com/rabith/string/StringOperation.java
+++ String/src/com/rabith/string/StringOperation.java
@@ -1,14 +1,40 @@
 package com.rabith.string;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringOperation {
+	/**
+	 * Reverse String
+	 * 
+	 * @param text
+	 * @return
+	 */
 	public String reverseString(String text) {
-		//String --> o(N*N)  StringBuilder --> o(N)
-		int lenghtOfString = text.length(); //o(1)
+		// String --> o(N*N) StringBuilder --> o(N)
+		int lenghtOfString = text.length(); // o(1)
 		StringBuilder revversedString = new StringBuilder();
-		
-		for(int index = lenghtOfString-1 ; index >= 0; index--) {//o(N)
-			revversedString.append(text.charAt(index)); //o(1)
+
+		for (int index = lenghtOfString - 1; index >= 0; index--) {// o(N)
+			revversedString.append(text.charAt(index)); // o(1)
 		}
 		return revversedString.toString();
+	}
+
+	/**
+	 * getSuffixes
+	 * 
+	 * @param text
+	 * @return
+	 */
+	public List<String> getSuffixes(String text) {
+
+		int lengthOfText = text.length();// O(1);
+		List<String> suffixesList = new ArrayList<>();
+
+		for (int index = 0; index < lengthOfText; ++index) {
+			suffixesList.add(text.substring(index, lengthOfText)); // O(1)
+		}
+		return suffixesList;
 	}
 }
